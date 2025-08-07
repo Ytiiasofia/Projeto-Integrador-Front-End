b02d52b4e18f50ca540b9bbd153ffea2ff292446
Sofia Müller David <sofiamuller1201@gmail.com>
2025-08-07 10:36:47 -0300

    Adição do Include scriptScr.php

--- Back-End Projeto Integrador/UserAnonimo/about.php
+++ Back-End Projeto Integrador/UserAnonimo/about.php
@@ -110,17 +110,9 @@
   <!-- Pré-carregamento -->
   <div id="preloader"></div>
 
-  <!-- Arquivos JS de fornecedores -->
-  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../../assets/vendor/php-email-form/validate.js"></script>
-  <script src="../../assets/vendor/aos/aos.js"></script>
-  <script src="../../assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="../../assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="../../assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-  <!-- Arquivo JS principal -->
-  <script src="../../assets/js/main.js"></script>
+  <?php
+    require("../Include/scriptScr.php");
+  ?>
 
 </body>
 
--- Back-End Projeto Integrador/UserAnonimo/services.php
+++ Back-End Projeto Integrador/UserAnonimo/services.php
@@ -282,18 +282,9 @@
         </div>
     </div>
 
-    <!-- JavaScript de Terceiros -->
-    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/vendor/php-email-form/validate.js"></script>
-    <script src="../assets/vendor/aos/aos.js"></script>
-    <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="../assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-    <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-    <!-- JavaScript Principal -->
-    <script src="../assets/js/main.js"></script>
-
+    <?php
+        require("../Include/scriptScr.php");
+    ?>
     <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Modal para Publicar
