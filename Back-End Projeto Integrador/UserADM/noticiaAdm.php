5b0a4a0e13df5266546dc2d9cf3021c0cf286f18
Sofia Müller David <sofiamuller1201@gmail.com>
2025-08-14 10:31:54 -0300

    salvando alterações

--- Back-End Projeto Integrador/UserADM/noticiaAdm.php
+++ Back-End Projeto Integrador/UserADM/noticiaAdm.php
@@ -76,11 +76,27 @@
 </head>
 
 <body class="blog-page">
-  <!-- Header -->
-  <?php
-    require("../Include/menuADM.php");
-  ?>
-  
+
+  <!-- Cabeçalho -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+      <a class="logo d-flex align-items-center">
+        <h1 class="sitename">She Innovates</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+                <ul>
+                <li><a href="controleUserAdm.php"  >Gerenciamento de Usuários</a></li>
+                <li><a href="noticiaAdm.php"class="active">Notícias</a></li>
+                <li><a href="oportunidadeAdm.php">Oportunidades</a></li>
+                <li><a href="forumAdm.php" >Fórum</a></li>
+                <li><a href="perfilAdm.php" >Perfil</a></li>
+                </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+    </div>
+  </header>
+
   <main class="main">
 
     <!-- Page Title -->
--- Back-End Projeto Integrador/UserADM/oportunidadeAdm.php
+++ Back-End Projeto Integrador/UserADM/oportunidadeAdm.php
@@ -55,10 +55,24 @@
 </head>
 
 <body class="portfolio-page">
-  <!-- Header -->
-  <?php
-    require("../Include/menuADM.php");
-  ?>
+
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+            <a class="logo d-flex align-items-center">
+                <h1 class="sitename">She Innovates</h1>
+            </a>
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                <li><a href="controleUserAdm.php">Gerenciamento de Usuários</a></li>
+                <li><a href="noticiaAdm.php">Notícias</a></li>
+                <li><a href="oportunidadeAdm.php" class="active">Oportunidades</a></li>
+                <li><a href="forumAdm.php">Fórum</a></li>
+                <li><a href="perfilAdm.php">Perfil</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+        </div>
+    </header>
 
     <main class="main">
         <!-- Título da Página -->
