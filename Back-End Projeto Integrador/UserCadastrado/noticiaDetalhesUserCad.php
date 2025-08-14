83c356d37e056d3e2f8b11b2f38c3c23026080df
Sofia Müller David <sofiamuller1201@gmail.com>
2025-08-14 10:45:41 -0300

    Adição do require (../Include/footerUserAnom.php) nos arquivos da pasta UserCadastrado

--- Back-End Projeto Integrador/UserCadastrado/noticiaDetalhesUserCad.php
+++ Back-End Projeto Integrador/UserCadastrado/noticiaDetalhesUserCad.php
@@ -248,42 +248,9 @@
   </main>
 
  <footer id="footer" class="footer light-background">
-    <div class="footer-top">
-        <div class="container">
-            <div class="row gy-4">
-                <!-- Logo e descrição (EXTREMA ESQUERDA) -->
-                <div class="col-lg-6 col-md-12 footer-about">
-                    <a href="inicioUserCad.html" class="logo d-flex align-items-center">
-                        <span class="sitename">She Innovates</span>
-                    </a>
-                    <p>Acreditamos na força da comunidade para enfrentar desafios, incentivar permanências e abrir portas.</p>
-                    <div class="social-links d-flex mt-4">
-                        <a href=""><i class="bi bi-twitter-x"></i></a>
-                        <a href=""><i class="bi bi-facebook"></i></a>
-                        <a href=""><i class="bi bi-instagram"></i></a>
-                        <a href=""><i class="bi bi-linkedin"></i></a>
-                    </div>
-                </div>
-
-                <!-- Contato (EXTREMA DIREITA) -->
-                <div class="col-lg-6 col-md-12 footer-contact text-end">
-                    <h4>Entre em Contato</h4>
-                    <p>Rua Ernesto Alves 1942</p>
-                    <p>Taquara, RS</p>
-                    <p>Brasil</p>
-                    <p class="mt-4"><strong>Telefone:</strong> <span>(11) 4002-8922</span></p>
-                    <p><strong>E-mail:</strong> <span>SheInnovates@gmail.com</span></p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container copyright text-center">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">Nova</strong> <span>All Rights Reserved</span></p>
-        <div class="credits">
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-    </div>
+  <?php
+    require("../Include/footerUserAnom.php");
+  ?>
 </footer>
 
   <!-- Scroll Top -->
